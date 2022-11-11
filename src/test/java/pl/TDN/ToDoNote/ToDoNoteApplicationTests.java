40922183016113479ba84dd32862a2898fdc8951
Adam4531 <70113220+Adam4531@users.noreply.github.com>
2022-11-11 18:41:44 +0100

    package location fixed

---
+++ src/test/java/pl/TDN/ToDoNote/ToDoNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.TDN.ToDoNote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
